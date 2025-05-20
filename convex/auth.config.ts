ce08672d30b8a4ee8052b5331423c7cc4b4d9d09
Subhamay-Dey <ncdey1966@gmail.com>
2025-05-20 13:51:10 +0530

    Added Convex schema definition with users and plans tables ✅

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,7 +1,7 @@
 export default {
   providers: [
     {
-      domain: process.env.NEXT_PUBLIC_CLERK_FRONTEND_API_URL as string,
+      domain: "https://finer-gar-20.clerk.accounts.dev",
       applicationID: "convex",
     },
   ]
