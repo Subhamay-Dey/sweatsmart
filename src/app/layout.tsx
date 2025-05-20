5775aad49fa04c2872559ae3c7b804bb012550c3
Subhamay-Dey <ncdey1966@gmail.com>
2025-05-20 09:33:43 +0530

    Integrated Convex with Clerk authentication by adding ConvexProviderWithClerk ✅

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,9 @@
-import {
-  ClerkProvider,
-  SignInButton,
-  SignUpButton,
-  SignedIn,
-  SignedOut,
-  UserButton,
-} from '@clerk/nextjs'
+import { ClerkProvider } from '@clerk/nextjs'
 
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import ConvexClientProvider from '@/provider/ConvexClerkProvider';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -32,14 +26,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
-      <html lang="en">
+    <html lang="en">
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
           >
-          {children}
+            <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY as string}>
+              <ConvexClientProvider>
+                {children}
+              </ConvexClientProvider>
+            </ClerkProvider>
         </body>
       </html>
-    </ClerkProvider>
   );
 }
