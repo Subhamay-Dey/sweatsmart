9c48c7c0ec686252bdf981e00d71729591652ed5
Subhamay-Dey <ncdey1966@gmail.com>
2025-05-21 06:27:10 +0530

    Completed Navbar and Background part ✅

---
+++ src/components/Background.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Background() {
+  return (
+    <div className="fixed inset-0 -z-1">
+        <div className="absolute inset-0 bg-gradient-to-b from-background via-background to-background"></div>
+        <div className="absolute inset-0 bg-[linear-gradient(var(--cyber-grid-color)_1px,transparent_1px),linear-gradient(90deg,var(--cyber-grid-color)_1px,transparent_1px)] bg-[size:20px_20px]"></div>
+    </div>
+  )
+}
+
+export default Background
